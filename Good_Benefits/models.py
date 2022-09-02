648483c7a4ff45a2f98ae6aa9f3b61245e7e7c90
LimDongHwi <dlaehdgnl02@gmail.com>
2022-09-02 23:22:17 +0900

    ver 0.01

--- Good_Benefits/models.py
+++ Good_Benefits/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Feed(models.Model):
+    content = models.TextField()
+    image = models.TextField()
+    profile_image = models.TextField()
+    user_id = models.TextField()
+    view_count = models.IntegerField()
+
+
+class RecommendPeople(models.Model):
+    user_id = models.TextField()
+    profile_image = models.TextField()
+    comment = models.TextField()
--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Good_Benefits.apps.GoodBenefitsConfig',
+    'Good_Benefits',
+
 ]
 
 MIDDLEWARE = [
