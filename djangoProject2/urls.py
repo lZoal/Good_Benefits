1063cf1edd23fcf6745f738ca4eb66dbb627614c
LimDongHwi <dlaehdgnl02@gmail.com>
2022-09-03 13:49:10 +0900

    ver 0.04 발표임시

--- djangoProject2/urls.py
+++ djangoProject2/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import Main
-
+from django.contrib import admin
 urlpatterns = [
-    path('', Main.as_view())
+    path('', Main.as_view()),
+    path('admin/', admin.site.urls),
 ]
